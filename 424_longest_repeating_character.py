567799496dce416e57f8d96dffeaa5b3fa9d49ba
Manuel <juanzatch17@hotmail.com>
2025-02-13 22:44:18 -0500

    Revisit 424

--- 424_longest_repeating_character.py
+++ 424_longest_repeating_character.py
@@ -5,23 +5,25 @@ k = 1
 # 2. Res is never higher than the len of s 
 # 3. We can use a hashmap to store the frequency of each letter
 
-charCount = {}
-maxFreq = 0
-left = 0
+counter = {}
+maxFreq = 0 
+left = 0 
 maxLen = 0
-
 for right in range(len(s)):
-    # If it exists return it, else return 0 
-    charCount[s[right]] = charCount.get(s[right], 0) + 1
-    maxFreq = max(maxFreq, charCount[s[right]])
+    # set it to 0 if it doesn't exist, else set it to it's value + 1
+    counter[s[right]] = counter.get(s[right], 0) + 1
+    # check if it's the current longest substring
+    maxFreq = max(maxFreq, counter[s[right]])
     
-    # Right - left + 1 is the size of the window 
-    # maxFreq is the current Longest substring
-    # If at some point this is higher than k. Example in AABABBA K= 1. When {A=3, B=2} -> Max freq is 3, win size is 5 > k
-    # This equation represents the number of replacement needed 
+    
+    # We remove the most frequent letter from the substring. If that amount exceeds k it means 
+    # we have violated the condition of more than k flips    
     while (right - left + 1) - maxFreq > k:
-        charCount[s[left]] -= 1
-        left +=1
-        
+        # If that's the case we have to reduce the window by deleting left letter from dict
+        counter[s[left]] -= 1
+        # we also need look to the right of the array 
+        left += 1 
+    # We calculate maxLen by comparing the current max len with the distance between left and right after the
+    # changes
     maxLen = max(maxLen, right - left + 1)
-print(maxLen)
+    
